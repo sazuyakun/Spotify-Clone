b4b34558dbd2b69402756d05dd5fcff6edad051b
Soham Samal <sohamsamal37@gmail.com>
2024-01-16 06:55:38 +0530

    Connection to Mongo is successful

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,9 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
 const app = express();
+import 'dotenv/config'
 const port = 3000;
 
+//connecting mongodb to node app
+MongoClient.connect("mongodb+srv://user2002:" +
+ process.env.PASSWORD +
+ "@cluster0.6hmrs4w.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ })
+ .then((x) => {
+    console.log("Connected to Mongo.")
+ })
+ .catch((err) => {
+    console.log("Error while connecting to mongo")
+ });
+
 app.get("/", (req, res) => {
-    res.send("Hello urvi")
+    res.send("Hello World")
 })
 
 app.listen(port, () => {
